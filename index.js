8ea9b1d100fb7f82063fa58c200d565afd7cbc1d
Sandeepkumar Parekh <sandeepparekh9@gmail.com>
2017-07-28 10:53:34 +1000

    updates

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ app.get('/dummyget', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ 'speech': 'dummy speech', 'displayText':'dummy get works!' }));
 })
+
+
+app.listen((process.env.PORT || 5000), function () {
+    console.log("Server listening" );
+});
