bf99c44a1f325d0918b569584eda9ddabc8b5bfe
Sandeepkumar Parekh <sandeepparekh9@gmail.com>
2017-07-29 19:11:28 +1000

    converted to text message from image

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function callRecipePuppy(fooditem) {
 
                 let output = "Found a recipe for: " + firstItem.title + ". Go to:  " + firstItem.href;
                 var obj = {
-                    caption: '<b><Title:/b> ' + firstItem.title + '\n\n' + '<b>Ingredients:</b> ' + firstItem.ingredients + '\n\n' + '<b>Link:</b> ' + firstItem.href,
+                    text: '<b><Title:/b> ' + firstItem.title + '\n' + '<b>Ingredients:</b> ' + firstItem.ingredients + '\n' + '<b>Link:</b> ' + firstItem.href,
                     photo: firstItem.thumbnail,
                     parse_mode: 'HTML'
                 }
